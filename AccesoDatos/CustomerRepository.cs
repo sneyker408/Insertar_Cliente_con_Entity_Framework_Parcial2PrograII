413c06efb90e4c5e12e1039c8b09031dc716ca74
sneyker_408 <sneyker408@hotmail.com>
2024-10-08 23:08:24 -0600

    4. Insertar Cliente con Entity Framework

---
+++ AccesoDatos/CustomerRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    public class CustomerRepository
+    {
+        public NorthwindEntities contexto = new NorthwindEntities();
+
+        public List<Categories> CargarInformacion()
+        {
+            var categorias = from Category in contexto.Categories
+                           select Category;
+
+            return categorias.ToList();
+
+        }
+
+        public int InsertarCategoria(Categories categorias)
+        {
+            contexto.Categories.Add(categorias);
+            return contexto.SaveChanges();
+        }
+    }
+
+}
